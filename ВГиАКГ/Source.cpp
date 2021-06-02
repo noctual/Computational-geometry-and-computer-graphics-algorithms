81367a2960bb6663172256a2ba6af62ce6b95fff
ivans592@yandex.ru <60885752+noctual@users.noreply.github.com>
2021-06-02 09:41:32 +0500

    Лабораторная 1 | Пример 1

--- ВГиАКГ/Source.cpp
+++ ВГиАКГ/Source.cpp
@@ -6,26 +6,189 @@ using namespace std;
 
 GLFWwindow* g_window;
 
-void init()
+GLuint g_shaderProgram;
+
+class Model
 {
-    // Set initial color of color buffer to blue.
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+public:
+    GLuint vbo;
+    GLuint ibo;
+    GLuint vao;
+    GLsizei indexCount;
+};
+
+Model g_model;
+
+GLuint createShader(const GLchar* code, GLenum type)
+{
+    GLuint result = glCreateShader(type);
+
+    glShaderSource(result, 1, &code, NULL);
+    glCompileShader(result);
+
+    GLint compiled;
+    glGetShaderiv(result, GL_COMPILE_STATUS, &compiled);
+
+    if (!compiled)
+    {
+        GLint infoLen = 0;
+        glGetShaderiv(result, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 0)
+        {
+            char* infoLog = (char*)alloca(infoLen);
+            glGetShaderInfoLog(result, infoLen, NULL, infoLog);
+            cout << "Shader compilation error" << endl << infoLog << endl;
+        }
+        glDeleteShader(result);
+        return 0;
+    }
+
+    return result;
+}
+
+GLuint createProgram(GLuint vsh, GLuint fsh)
+{
+    GLuint result = glCreateProgram();
+
+    glAttachShader(result, vsh);
+    glAttachShader(result, fsh);
+
+    glLinkProgram(result);
+
+    GLint linked;
+    glGetProgramiv(result, GL_LINK_STATUS, &linked);
+
+    if (!linked)
+    {
+        GLint infoLen = 0;
+        glGetProgramiv(result, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 0)
+        {
+            char* infoLog = (char*)alloca(infoLen);
+            glGetProgramInfoLog(result, infoLen, NULL, infoLog);
+            cout << "Shader program linking error" << endl << infoLog << endl;
+        }
+        glDeleteProgram(result);
+        return 0;
+    }
+
+    return result;
+}
+
+bool createShaderProgram()
+{
+    g_shaderProgram = 0;
+
+    const GLchar vsh[] =
+        "#version 330\n"
+        ""
+        "layout(location = 0) in vec2 a_position;"
+        "layout(location = 1) in vec2 a_params;"
+        ""
+        "out vec2 v_params;"
+        ""
+        "void main()"
+        "{"
+        "    v_params = a_params;"
+        "    gl_Position = vec4(a_position, 0.0, 1.0);"
+        "}"
+        ;
+
+    const GLchar fsh[] =
+        "#version 330\n"
+        ""
+        "in vec2 v_params;"
+        ""
+        "layout(location = 0) out vec4 o_color;"
+        ""
+        "void main()"
+        "{"
+        "   o_color = vec4(v_params[0], 0.0, 0.0, 1.0);"
+        "}"
+        ;
+
+    GLuint vertexShader, fragmentShader;
+
+    vertexShader = createShader(vsh, GL_VERTEX_SHADER);
+    fragmentShader = createShader(fsh, GL_FRAGMENT_SHADER);
+
+    g_shaderProgram = createProgram(vertexShader, fragmentShader);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return g_shaderProgram != 0;
+}
+
+bool createModel()
+{
+    const GLfloat vertices[] =
+    {
+        -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f, 0.0f, 0.0f,
+    };
+
+    const GLuint indices[] =
+    {
+        0, 1, 2, 2, 3, 0
+    };
+
+    glGenVertexArrays(1, &g_model.vao);
+    glBindVertexArray(g_model.vao);
+
+    glGenBuffers(1, &g_model.vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, g_model.vbo);
+    glBufferData(GL_ARRAY_BUFFER, 20 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &g_model.ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_model.ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLuint), indices, GL_STATIC_DRAW);
+    g_model.indexCount = 6;
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (const GLvoid*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (const GLvoid*)(2 * sizeof(GLfloat)));
+
+    return g_model.vbo != 0 && g_model.ibo != 0 && g_model.vao != 0;
+}
+
+bool init()
+{
+    // Set initial color of color buffer to white.
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    return createShaderProgram() && createModel();
 }
 
 void reshape(GLFWwindow* window, int width, int height)
 {
-
+    glViewport(0, 0, width, height);
 }
 
 void draw()
 {
     // Clear color buffer.
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(g_shaderProgram);
+    glBindVertexArray(g_model.vao);
+
+    glDrawElements(GL_TRIANGLES, g_model.indexCount, GL_UNSIGNED_INT, NULL);
 }
 
 void cleanup()
 {
-
+    if (g_shaderProgram != 0)
+        glDeleteProgram(g_shaderProgram);
+    if (g_model.vbo != 0)
+        glDeleteBuffers(1, &g_model.vbo);
+    if (g_model.ibo != 0)
+        glDeleteBuffers(1, &g_model.ibo);
+    if (g_model.vao != 0)
+        glDeleteVertexArrays(1, &g_model.vao);
 }
 
 bool initOpenGL()
@@ -87,18 +250,21 @@ int main()
         return -1;
 
     // Initialize graphical resources.
-    init();
+    bool isOk = init();
 
-    // Main loop until window closed or escape pressed.
-    while (glfwGetKey(g_window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(g_window) == 0)
+    if (isOk)
     {
-        // Draw scene.
-        draw();
+        // Main loop until window closed or escape pressed.
+        while (glfwGetKey(g_window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(g_window) == 0)
+        {
+            // Draw scene.
+            draw();
 
-        // Swap buffers.
-        glfwSwapBuffers(g_window);
-        // Poll window events.
-        glfwPollEvents();
+            // Swap buffers.
+            glfwSwapBuffers(g_window);
+            // Poll window events.
+            glfwPollEvents();
+        }
     }
 
     // Cleanup graphical resources.
@@ -107,5 +273,5 @@ int main()
     // Tear down OpenGL.
     tearDownOpenGL();
 
-    return 0;
+    return isOk ? 0 : -1;
 }
